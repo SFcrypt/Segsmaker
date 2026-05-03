1c7c29064f670bba858254c78e72ec2c9b1c7a64
Santiago <136390230+SFcrypt@users.noreply.github.com>
2026-05-03 01:53:59 -0600

    Update cloning and completion messages in Updater.py

--- config/Updater.py
+++ config/Updater.py
@@ -11,34 +11,30 @@ def run_update():
     os.makedirs(f"{base}/dlbackend", exist_ok=True)
     os.makedirs(f"{base}/Models/diffusion_models", exist_ok=True)
 
-    say("<b>【{red} Cloning ComfyUI{d} 】{red}</b>")
     subprocess.run([
         "git", "clone",
         "https://github.com/SFcrypt/ComfyUI",
         f"{base}/dlbackend/ComfyUI"
     ])
 
-    say("<b>【{red} Cloning SwarmUI{d} 】{red}</b>")
     subprocess.run([
         "git", "clone",
         "https://github.com/SFcrypt/SwarmUI",
         f"{base}/SwarmUI_tmp"
     ])
 
-    say("<b>【{red} Copying Files{d} 】{red}</b>")
     subprocess.run(
         f"cp -r {base}/SwarmUI_tmp/* {base}/",
         shell=True
     )
 
-    say("<b>【{red} Cleaning{d} 】{red}</b>")
     subprocess.run(
         f"rm -rf {base}/SwarmUI_tmp",
         shell=True
     )
 
     clear_output()
-    say("<b>【{red} Listo{d} 】{red}</b>")
+    say("<b>【{red} Done{d} 】{red}</b>")
 
 if __name__ == "__main__":
     run_update()
